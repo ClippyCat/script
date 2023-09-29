57a432694b42bea6deed04385c20fea5378f3a2d
melody580085 <melody580085@gmail.com>
2023-09-29 11:18:34 -0600

    small change

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Scripts
 A collection of scripts to make my life easier.
+
+## sync
+* Sync local repo on windows to vps.
+* My simple salution to replace rcync.
+
+
--- sync.bat
+++ sync.bat
@@ -1,7 +1,3 @@
-# Sync local repo on windows to vps.
-# My simple salution to replace rcync.
-
-
 cd C:\Users\Melody\Desktop\bs\programming\clippycat.ca
 git push -u origin main
 ssh debian@clippycat.ca "cd /var/www/html; git pull origin main --rebase"
