306dfe5d6ffde649a3b9d97b85bc773886be9e97
melody580085 <melody580085@gmail.com>
2023-10-06 14:02:01 -0600

    update server

--- sync.bat
+++ sync.bat
@@ -1,3 +1,3 @@
 cd C:\Users\Melody\Desktop\bs\programming\clippycat.ca
 git push -u origin main
-ssh debian@clippycat.ca "cd /var/www/html; git pull origin main --rebase"
+ssh arch@clippycat.ca "cd /var/www/html; git pull origin main --rebase"
